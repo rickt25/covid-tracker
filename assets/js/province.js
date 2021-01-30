d126817fb1a12a40bbdcb7221f0ca3306a3f4ce0
rickt25 <derricktio36@gmail.com>
2021-01-30 18:56:18 +0700

    final commit

---
+++ assets/js/province.js
@@ -0,0 +1,86 @@
+
+window.addEventListener('load', () => {
+    const preload = document.getElementById('preloader')
+    setInterval(function () {
+        preload.classList.add('load-finish')
+    }, 1000)
+})
+
+async function getProvince() {
+    let url = 'https://indonesia-covid-19.mathdro.id/api/provinsi'
+    try {
+        let res = await fetch(url)
+        const { data } = await res.json()
+        localStorage.setItem("provinces", JSON.stringify(data))
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// let datas =  // data dummy karena api sedang down
+// {
+//     "data": [
+//         {
+//             "fid": 11,
+//             "kodeProvi": 31,
+//             "provinsi": "DKI Jakarta",
+//             "kasusPosi": 123,
+//             "kasusSemb": 321,
+//             "kasusMeni": 312,
+//         },
+//         {
+//             "fid": 12,
+//             "kodeProvi": 32,
+//             "provinsi": "Jawa Barat",
+//             "kasusPosi": 123,
+//             "kasusSemb": 321,
+//             "kasusMeni": 312,
+//         }
+//     ]
+// }
+
+async function renderProvince(key = "") {
+    const provinces = document.querySelector('#provinces');
+    const datas = localStorage.getItem("provinces") === undefined
+        ? await getProvince()
+        : JSON.parse(localStorage.getItem("provinces"))
+
+    datas.filter(province => province.provinsi.toLowerCase().match(key.toLowerCase())).forEach(data => {
+        const element = `
+                <div class="box-wrapper shadow">
+                     <div class="city">${data.provinsi}</div>
+                     <div class="positive">
+                         <p class="description">Positive</p>
+                         <p class="number">${data.kasusPosi}</p>
+                     </div>
+                     <div class="recovered">
+                         <p class="description">Recovered</p>
+                         <p class="number">${data.kasusSemb}</p>
+                     </div>
+                     <div class="death">
+                         <p class="description">Death</p>
+                         <p class="number">${data.kasusMeni}</p>
+                     </div>
+                 </div>
+                 `;
+        provinces.innerHTML += element
+    })
+}
+
+function clearProvince() {
+    document.querySelectorAll('.box-wrapper').forEach(e => e.remove());
+}
+
+
+function filterProvince() {
+    const searchInput = document.querySelector('#searchInput');
+    const key = searchInput.value;
+
+    if (!key) return renderProvince();
+
+    clearProvince();
+    renderProvince(key)
+}
+
+renderProvince()
---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const total = document.getElementById('total-cases')
+const positive = document.getElementById('positive')
+const recovered = document.getElementById('recovered')
+const death = document.getElementById('death')
+const date = document.getElementById('date')
+
+
+
+window.addEventListener('load', () => {
+    const preload = document.getElementById('preloader')
+    setInterval(function(){
+        preload.classList.add('load-finish')
+    }, 1000)
+})
+
+async function getData() {
+    let url = 'https://indonesia-covid-19.mathdro.id/api/'
+    try {
+        let res = await fetch(url)
+        return await res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function renderData(){
+    let data = await getData()
+
+    console.log(data)
+    
+    total.innerHTML = data.jumlahKasus
+    positive.innerHTML = data.perawatan
+    recovered.innerHTML = data.sembuh
+    death.innerHTML = data.meninggal
+    date.innerHTML = "Last updated : " + formatDate(data.lastUpdate.toString())
+}
+
+
+
+function formatDate(date){
+    let day = date.substring(0,2)
+    let month = date.substring(2,4)
+    let year = "20" + date.substring(4,6)
+    return day + "/" + month + "/" + year
+}
+
+renderData()
+
